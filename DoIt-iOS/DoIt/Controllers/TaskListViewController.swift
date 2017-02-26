a8e804490f1aae333bbfa0c0a3829db02ee5a604
Wang, Michael <ch_w@live.com>
2017-02-26 14:52:25 +0800

    add table view in the task list

--- DoIt-iOS/DoIt/Controllers/TaskListViewController.swift
+++ DoIt-iOS/DoIt/Controllers/TaskListViewController.swift
@@ -10,26 +10,33 @@ import UIKit
 
 class TaskListViewController: UIViewController {
 
+    @IBOutlet weak var taskListTableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        taskListTableView.estimatedRowHeight = 60
+        taskListTableView.rowHeight = UITableViewAutomaticDimension
+        taskListTableView.dataSource = self
+        taskListTableView.delegate = self
+        taskListTableView.register(UITableViewCell.self, forCellReuseIdentifier: "")
+    }
+}
+
+extension TaskListViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
-    */
+}
+
+extension TaskListViewController: UITableViewDelegate {
 
 }
---
+++ DoIt-iOS/DoIt/Services/TaskService.swift
@@ -0,0 +1,13 @@
+//
+//  TaskService.swift
+//  DoIt
+//
+//  Created by Wang, Michael on 26/02/2017.
+//  Copyright © 2017 MW. All rights reserved.
+//
+
+import UIKit
+
+class TaskService: NSObject {
+
+}
